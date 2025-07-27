3ffbd4984c3b3dcc5626397a43cb522b5465473a
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-07-27 10:32:11 +0500

    method signatures werer added to the interface

--- src/BookStore.Application/Interfaces/IBookImageRepository.cs
+++ src/BookStore.Application/Interfaces/IBookImageRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace BookStore.Application.Interfaces;
+﻿using BookStore.Domain.Entities;
+
+namespace BookStore.Application.Interfaces;
 
 public interface IBookImageRepository
 {
-    
+    Task<BookImage?> GetByIdAsync(long bookImageId);
+    Task<List<BookImage>> GetByBookIdAsync(long bookId);
+    Task<List<BookImage>> GetAllAsync();
+    Task AddAsync(BookImage image);
+    Task UpdateAsync(BookImage image);
+    Task DeleteAsync(BookImage image);
 }
