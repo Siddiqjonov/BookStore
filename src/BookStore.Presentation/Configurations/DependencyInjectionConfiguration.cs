d994246119b9213725c36e3c26f69b02913013b2
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-07-27 13:52:13 +0500

    Repositroies were registered to DI container

---
+++ src/BookStore.Presentation/Configurations/DependencyInjectionConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using BookStore.Application.Interfaces;
+using BookStore.Infrastructure.Persistence.Repositories;
+
+namespace BookStore.Presentation.Configurations;
+
+public static class DependencyInjectionConfiguration
+{
+    public static void RegisterRepositories(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IAddressRepository, AddressRepository>();
+        builder.Services.AddScoped<IBasketItemRepository, BasketItemRepository>();
+        builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+        builder.Services.AddScoped<IBookCategoryRepository, BookCategoryRepository>();
+        builder.Services.AddScoped<IBookImageRepository, BookImageRepository>();
+        builder.Services.AddScoped<IBookRatingRepository, BookRatingRepository>();
+        builder.Services.AddScoped<IBookRepository, BookRepository>();
+        builder.Services.AddScoped<IEmailVarificationCodeRepository, EmailVarificationCodeRepository>();
+        builder.Services.AddScoped<IFavoriteRepository, FavoriteRepository>();
+        builder.Services.AddScoped<IOrderHistoryRepository, OrderHistoryRepository>();
+        builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+        builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+        builder.Services.AddScoped<IOrderStatusRepository, OrderStatusRepository>();
+        builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        builder.Services.AddScoped<IRequestStatusRepository, RequestStatusRepository>();
+        builder.Services.AddScoped<IStoreCreationRequestRepository, StoreCreationRequestRepository>();
+        builder.Services.AddScoped<IStoreRepository, StoreRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository> ();
+        builder.Services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+    }
+}
