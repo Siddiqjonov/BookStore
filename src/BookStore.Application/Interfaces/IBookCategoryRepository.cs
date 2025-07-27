538b664911daa85932e5133196465ace3384e6a7
Saidabror Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2025-07-27 10:28:32 +0500

    method were  addded to IBookCategoryRepository

--- src/BookStore.Application/Interfaces/IBookCategoryRepository.cs
+++ src/BookStore.Application/Interfaces/IBookCategoryRepository.cs
@@ -1,5 +1,12 @@
-﻿namespace BookStore.Application.Interfaces;
+﻿using BookStore.Domain.Entities;
+
+namespace BookStore.Application.Interfaces;
 
 public interface IBookCategoryRepository
 {
+    Task<BookCategory?> GetByIdAsync(long bookCategoryId);
+    Task<List<BookCategory>> GetAllAsync();
+    Task AddAsync(BookCategory category);
+    Task UpdateAsync(BookCategory category);
+    Task DeleteAsync(BookCategory category);
 }
